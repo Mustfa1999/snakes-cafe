3471076e86fd84868c0a39d94f3ce69885205dce
Mustfa Ayyed Alhasanat <mustfaaayyed@gmail.com>
2022-02-27 21:16:53 +0300

    first commit

---
+++ snakes_cafe/snakes_cafe.py
@@ -0,0 +1,71 @@
+greetings = """
+$ python snakes_cafe.py
+**************************************
+**    Welcome to the Snakes Cafe!   **
+**    Please see our menu below.    **
+**
+** To quit at any time, type "quit" **
+**************************************
+
+Appetizers
+----------
+Wings
+Cookies
+Spring Rolls
+
+Entrees
+-------
+Salmon
+Steak
+Meat Tornado
+A Literal Garden
+
+Desserts
+--------
+Ice Cream
+Cake
+Pie
+
+Drinks
+------
+Coffee
+Tea
+Unicorn Tears
+
+***********************************
+** What would you like to order? **
+***********************************
+
+>> Enter "quit" to exit 
+
+>> Enter "meal" to display your order
+
+"""
+print(greetings)
+
+items = ["Wings", "Cookies", "Spring Rolls", "Salmon", "Steak", "Meat Tornado", "A Literal Garden", "Ice Cream", "Cake", "Pie", "Coffee", "Tea", "Unicorn Tears"]
+quantities = {item:0 for item in items}
+
+while True:
+    value = input("> ")
+
+    if value in items:
+        quantities[value] += 1
+        print(f"\n** {quantities[value]} order of {value} have been added to your meal **\n")
+    elif value == "quit":
+        break
+    elif value == "meal":
+        for i in [f"{key}: {val}" for key,val in quantities.items() if val!=0]:
+            print(i)
+    else:
+        print(f"Sorry ... \"{value}\" is not on the menu.\n")
+        while True:
+            answer = input("Would you like to order anything else ? (y/n) : ")
+            if (answer == "y") or (answer == "n"):
+                break
+            else:
+                print("Please select \"y\" or \"n\"\n")
+        if answer == "n":
+            break
+
+print("\nThanks for ordering !!\n")
